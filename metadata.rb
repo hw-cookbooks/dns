3a1aa666aac81ec8523d0ec9ea0c9f6f3a0acdf4
Chris Roberts <chrisroberts.code@gmail.com>
2013-06-21 10:50:27 -0700

    Bump version for development v0.0.3

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'chrisroberts.code@gmail.com'
 license 'Apache 2.0'
 description 'Create DNS entries for nodes'
 
-version '0.0.1'
+version '0.0.3'
 
 depends 'build-essential', '>= 1.1.0' # set minimum so we get compile time support

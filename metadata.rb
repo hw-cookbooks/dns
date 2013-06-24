25ec24dd656a7fa54ba22a5faa009c65460f7d74
Chris Roberts <chrisroberts.code@gmail.com>
2013-06-24 11:10:58 -0700

    Push hosts_file dependency restriction up to 0.1.4 minimum

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description 'Create DNS entries for nodes'
 
 version '0.0.5'
 
-depends 'hosts_file', '~> 0.1.2'
+depends 'hosts_file', '~> 0.1.4'
 depends 'build-essential', '>= 1.1.0' # set minimum so we get compile time support

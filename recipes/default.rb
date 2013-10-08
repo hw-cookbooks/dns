a44e03f0cd7c95e99b9626bce94f1f916c3e0055
Baldur <baldur@howaboutwe.com>
2013-10-08 16:39:10 -0400

    forking for aws

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,15 +13,23 @@ ruby_block 'Apply DNS information' do
     end
     con = Fog::DNS.new(Mash.new(:provider => node[:dns][:provider]).merge(node[:dns][:credentials].to_hash))
 
-    domain = con.list_domains.body['domains'].detect do |domain_hash|
-      domain_hash['name'] == node[:dns][:domain]
+    if node[:dns][:credentials][:provider] == "AWS"
+      domain = con.list_hosted_zones.data[:body]['HostedZones'].detect do |domain_hash|
+        domain_hash['Name'] == "#{node[:dns][:domain]}."
+      end
+      zone = con.zones.get(domain['Id'])
+    else
+      domain = con.list_domains.body['domains'].detect do |domain_hash|
+        domain_hash['name'] == node[:dns][:domain]
+      end
+      raise "Failed to locate registered domain in account for configured domain: #{node[:dns][:domain]}" unless domain
+      zone = con.zones.get(domain['id'])
+      raise "Failed to locate zone for configured domain: #{node[:dns][:domain]}" unless zone
+      record = zone.records.detect do |r|
+        r.name == node[:dns][:entry][:name]
+      end
     end
-    raise "Failed to locate registered domain in account for configured domain: #{node[:dns][:domain]}" unless domain
-    zone = con.zones.get(domain['id'])
-    raise "Failed to locate zone for configured domain: #{node[:dns][:domain]}" unless zone
-    record = zone.records.detect do |r|
-      r.name == node[:dns][:entry][:name]
-    end
+
     if(record)
       Chef::Log.info "DNS - Found existing record for: #{node[:dns][:entry][:name]}. Updating."
       Chef::Log.info "DNS - Existing type: #{record.type} Existing value: #{record.value}"

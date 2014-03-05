9962dded4efbc51eed415322efc56ae367fd98dd
Lopaka Delp <lopaka@rightscale.com>
2014-03-04 19:11:08 -0800

    Update fake recipe name.

--- test/cookbooks/fake/metadata.rb
+++ test/cookbooks/fake/metadata.rb
@@ -3,7 +3,7 @@ version          '0.1.0'
 
 depends 'dns'
 
-recipe 'fake::create_zone', 'Creates a DNS zone'
+recipe 'fake::zone', 'Creates a DNS zone'
 recipe 'fake::update', 'Updates a DNS entry'
 recipe 'fake::delete', 'Delete DNS entry'
 recipe 'fake::fog_mock', 'Turn on Fog mock'

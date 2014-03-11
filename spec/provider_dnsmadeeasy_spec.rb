e5a195812b042fa72335de37f3a72acb0c49458c
Lopaka Delp <lopaka@rightscale.com>
2014-03-11 01:18:54 -0700

    Additional dev on ChefSpec tests.

--- spec/provider_dnsmadeeasy_spec.rb
+++ spec/provider_dnsmadeeasy_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
-describe "DNSMadeEasy DNS provider" do
+describe 'fake::dme' do
 
   let(:provider) do
-    provider = Chef::Provider::Dnsmadeeasy.new(new_resource, run_context)
-    provider.stub(:create).and_return(client_stub)
+    provider = Chef::Provider::DnsDnsmadeeasyApi20.new(new_resource)
+    provider.stub.and_return()
     provider
+  end
+
+
+  let(:chef_run) do
+    runner = ChefSpec::Runner.new(step_into: ['dns'])
+    runner.node.set[:dns][:credentials] = {:dnsmadeeasy_api_key => "key", :dnsmadeeasy_secret_key => "secret"}
+    runner.node.set[:dns][:credentials] = {:dnsmadeeasy_api_key => "key", :dnsmadeeasy_secret_key => "secret"}
+    runner.node.set[:dns][:disable] = false
+    runner.converge(described_recipe)
+  end
+
+  it 'installs entry in dnsmadeeasy v2.0' do
+    expect(chef_run).to create_dns('DME Node DNS entry')
   end
 
 end

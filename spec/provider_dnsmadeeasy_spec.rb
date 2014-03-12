21ae3652be07d91b2044a61736b6dd621e1afa5a
Lopaka Delp <lopaka@rightscale.com>
2014-03-11 23:20:19 -0700

    Finish dev of chefspec for dnsmadeeasy_api20 provider

--- spec/provider_dnsmadeeasy_spec.rb
+++ spec/provider_dnsmadeeasy_spec.rb
@@ -24,19 +24,65 @@ describe 'fake::dme' do
     runner.converge(described_recipe)
   end
 
-  # TODO 
-  #  - add verification of auth_headers
-  #  - context block to handle if we need to update or add entry
-  #  - Check node[:dns][:disable] is enabled
+  let(:chef_run_dns_disable) do
+    runner = ChefSpec::Runner.new(step_into: ['dns'])
+    runner.node.set[:dns][:credentials] = {:dnsmadeeasy_api_key => "key", :dnsmadeeasy_secret_key => "secret"}
+    runner.node.set[:dns][:disable] = true
+    runner.node.set[:dns][:domain] = "test.com"
+    runner.node.set[:dns][:entry][:name] = "www"
+    runner.node.set[:dns][:entry][:value] = "10.0.0.1"
+    runner.node.set[:dns][:entry][:type] = "A"
+    runner.node.set[:dns][:entry][:ttl] = 60
+    runner.converge(described_recipe)
+  end
 
-  it 'installs entry in dnsmadeeasy v2.0' do
-    rest_client.should_receive(:get).exactly(2).and_return(
-      '{"data":[{"name":"test.com","id":334721,"created":1192147200000}]}',
-      '{"data":[{"name":"101test1403","value":"192.168.1.2","id":14101880,"type":"A","ttl":60}]}'
-    )
-    rest_client.should_receive(:put).and_return(nil)
-    rest_client.should_receive(:post).and_return(nil)
-    chef_run
+  # TODO - add verification of auth_headers
+
+  context "when a dns entry already exists for updating" do
+    it 'update entry in dnsmadeeasy v2.0' do
+      rest_client.should_receive(:get).exactly(2).and_return(
+        '{"data":[{"name":"test.com","id":123456,"created":1192147200000}]}',
+        '{"data":[{"name":"www","value":"192.168.1.2","id":12345678,"type":"A","ttl":60}]}'
+      )
+      auth_headers.should_receive
+      rest_client.should_receive(:put).and_return(nil)
+      chef_run
+    end
+  end
+
+  context "when a dns entry does not need updating" do
+    it 'update entry in dnsmadeeasy v2.0' do
+      rest_client.should_receive(:get).exactly(2).and_return(
+        '{"data":[{"name":"test.com","id":123456,"created":1192147200000}]}',
+        '{"data":[{"name":"www","value":"10.0.0.1","id":12345678,"type":"A","ttl":60}]}'
+      )
+      rest_client.should_not_receive(:post)
+      rest_client.should_not_receive(:put)
+      chef_run
+    end
+  end
+
+  context "when a dns entry should be created" do
+    it 'add entry in dnsmadeeasy v2.0' do
+      rest_client.should_receive(:get).exactly(2).and_return(
+        '{"data":[{"name":"test.com","id":123456,"created":1192147200000}]}',
+        '{"data":[]}'
+      )
+      rest_client.should_receive(:post).and_return(nil)
+      chef_run
+    end
+  end
+
+  context "when a dns entry does not exist and creation is disabled" do
+    it 'only update entry in dnsmadeeasy v2.0' do
+      rest_client.should_receive(:get).exactly(2).and_return(
+        '{"data":[{"name":"test.com","id":123456,"created":1192147200000}]}',
+        '{"data":[]}'
+      )
+      rest_client.should_not_receive(:post)
+      rest_client.should_not_receive(:put)
+      chef_run_dns_disable
+    end
   end
 
 end

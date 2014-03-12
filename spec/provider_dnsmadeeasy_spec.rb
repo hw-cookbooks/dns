1945520a7293134a11d0c4d511f3339404952cf2
Lopaka Delp <lopaka@rightscale.com>
2014-03-11 23:36:36 -0700

    Added chefspec for removal of dns entry using dnsmadeeasy_api20 provider

--- spec/provider_dnsmadeeasy_spec.rb
+++ spec/provider_dnsmadeeasy_spec.rb
@@ -44,7 +44,6 @@ describe 'fake::dme' do
         '{"data":[{"name":"test.com","id":123456,"created":1192147200000}]}',
         '{"data":[{"name":"www","value":"192.168.1.2","id":12345678,"type":"A","ttl":60}]}'
       )
-      auth_headers.should_receive
       rest_client.should_receive(:put).and_return(nil)
       chef_run
     end
@@ -85,4 +84,47 @@ describe 'fake::dme' do
     end
   end
 
+end
+
+describe 'fake::dme_delete' do
+
+  let(:rest_client) do
+    client_stub = double("RestClient::Resource")
+    client_stub.stub(:[]).and_return(client_stub)
+    client_stub
+  end
+
+  before(:each) do
+    RestClient::Resource.stub(:new).and_return(rest_client)
+  end
+
+  let(:chef_run) do
+    runner = ChefSpec::Runner.new(step_into: ['dns'])
+    runner.node.set[:dns][:credentials] = {:dnsmadeeasy_api_key => "key", :dnsmadeeasy_secret_key => "secret"}
+    runner.node.set[:dns][:domain] = "test.com"
+    runner.node.set[:dns][:entry][:name] = "www"
+    runner.converge(described_recipe)
+  end
+
+  context "when a dns entry exists to be deleted" do
+    it 'deleting entry in dnsmadeeasy v2.0' do
+      rest_client.should_receive(:get).exactly(2).and_return(
+        '{"data":[{"name":"test.com","id":123456,"created":1192147200000}]}',
+        '{"data":[{"name":"www","value":"192.168.1.2","id":12345678,"type":"A","ttl":60}]}'
+      )
+      rest_client.should_receive(:delete).and_return(nil)
+      chef_run
+    end
+  end
+
+  context "when a dns entry does not exist to be deleted" do
+    it 'not deleting entry that does not exist in dnsmadeeasy v2.0' do
+      rest_client.should_receive(:get).exactly(2).and_return(
+        '{"data":[{"name":"test.com","id":123456,"created":1192147200000}]}',
+        '{"data":[]}'
+      )
+      rest_client.should_not_receive(:delete)
+      chef_run
+    end
+  end
 end

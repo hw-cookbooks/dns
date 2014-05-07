151881c76f2567f4a8f014f0889f641cc86fd968
Lopaka Delp <lopaka@rightscale.com>
2014-05-06 22:34:16 -0700

    From code review, revert version to 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chrisroberts.code@gmail.com'
 license 'Apache 2.0'
 description 'Create DNS entries for nodes'
 
-version '0.1.4'
+version '0.1.3'
 
 depends 'hosts_file', '~> 0.1.4'
 depends 'build-essential', '>= 1.1.0' # set minimum so we get compile time support
